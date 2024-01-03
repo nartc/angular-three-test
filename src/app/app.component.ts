96011ba773ef6ec9f7e9056341226cd8ca814876
Chau Tran <nartc7789@gmail.com>
2024-01-03 09:45:32 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
+import { NgtCanvas } from 'angular-three';
+import { Experience } from './experience/experience.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    
-  `,
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, NgtCanvas],
+    template: `<ngt-canvas [sceneGraph]="scene" />`,
+    styles: [],
 })
 export class AppComponent {
-  title = 'angular-three-test';
+    scene = Experience;
+    title = 'angular-three-test';
 }
